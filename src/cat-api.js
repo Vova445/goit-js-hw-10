e2f551edb67a0a544561dcd8da91a6dd90b25835
Volodya Mikhnov <vvvmikhnov@gmail.com>
2023-10-24 21:42:11 +0300

    ten

--- src/cat-api.js
+++ src/cat-api.js
@@ -14,7 +14,7 @@ function fetchBreeds() {
   
   function fetchCatByBreed(breedId) {
     const BASE_URL = 'https://api.thecatapi.com/v1';
-    const API_KEY = 'live_YvBUC7pNaVN9iFaNNxOzRzSiJlefW0E3guhFWu0uXRwmA951TzOxA00DwbPam2pW';
+    const API_KEY = 'live_mI6LUQ2enmzcq4zLkkMN7ocuuKBZP22oz5U4YwbO8hFlVapQXu6oo54orzp0TeF0';
   
     return fetch(`${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`)
       .then(resp => {
