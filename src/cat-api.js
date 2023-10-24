e5f2de429f99764161fa5472091e2320fdac4e74
Volodya Mikhnov <vvvmikhnov@gmail.com>
2023-10-24 21:25:34 +0300

    nine

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,19 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 const BASE_URL = 'https://api.thecatapi.com/v1';
-const API_KEY =
-  'live_mI6LUQ2enmzcq4zLkkMN7ocuuKBZP22oz5U4YwbO8hFlVapQXu6oo54orzp0TeF0';
-
-axios.defaults.headers.common['x-api-key'] = API_KEY;
-axios.defaults.baseURL = BASE_URL;
+const API_KEY = 'live_mI6LUQ2enmzcq4zLkkMN7ocuuKBZP22oz5U4YwbO8hFlVapQXu6oo54orzp0TeF0';
 
 export function fetchBreeds() {
-  return axios.get('/breeds').then(response => {
-    return response.data;
-  });
-}
-
+   return axios
+        .get(`${BASE_URL}/breeds?key=${API_KEY}`)
+        .then(resp => resp.data)
+        
+    }
+    
 export function fetchCatByBreed(breedId) {
-  return axios.get(`/images/search?breed_ids=${breedId}`).then(resp => {
-    return resp.data;
-  });
+return axios
+   .get(`${BASE_URL}/images/search?key=${API_KEY}&breed_ids=${breedId}`)
+        .then(resp =>  resp.data)
 }
